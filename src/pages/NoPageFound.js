f660ce401e32483aed85290e513a1522a37c9538
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2021-11-05 19:13:39 +0100

    corrected some link-to routes

--- src/pages/NoPageFound.js
+++ src/pages/NoPageFound.js
@@ -5,7 +5,7 @@ export const NoPageFound = () => {
 	return (
 		<div className="uk-container uk-container-expanded" data-uk-height-viewport>
 			<h1 className="uk-text-center uk-margin-large-top uk-text-danger">Ooops Page not found</h1>
-			<p className="uk-text-center uk-h5"><Link to="/" className="uk-button uk-button-text uk-text-primary uk-text-bold">Go to Homepage</Link></p>
+			<p className="uk-text-center uk-h5"><Link to="/home" className="uk-button uk-button-text uk-text-primary uk-text-bold">Go to Homepage</Link></p>
 		</div>
 	)
 }
